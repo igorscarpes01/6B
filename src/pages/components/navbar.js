3a38c825d5bd6c791939595a704688587e5ea262
igorscarpes01 <igor.carpes+6b@estudante.ifms.edu.br>
2024-10-07 15:41:37 +0000

    Aula 07.10

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,3 +1,23 @@
-export default function Navbar(){
-    return <div>Menu</div>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary"  bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="/">6B</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default BasicExample;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
+import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import {Container, Row, Col} from 'react-bootstrap';
+
 
 function Index(){
-  return 
-  <>
+  return <>
     <Navbar />
-    <h1>Olá Mundo</h1>
+    <Container>
+      <Row>
+        <Col className="bg-primary">1 of 2</Col>
+        <Col className="bg-primary-subtle">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-danger">1 of 3</Col>
+        <Col className="bg-danger-subtle">2 of 3</Col>
+        <Col  className="grad">3 of 3</Col>
+      </Row>
+    </Container>
     <Footer />
   </>
 }
